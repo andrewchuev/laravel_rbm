9144ba21128bb6dcfe8bea72649c71ffca7839f1
Andrey Chuiev <andrey.chuiev@dteam.dev>
2023-04-01 15:12:39 +0300

    Update header logo

--- resources/views/brand/header.blade.php
+++ resources/views/brand/header.blade.php
@@ -1,18 +1,15 @@
 @push('head')
-    <meta name="robots" content="noindex" />
-    <link
-        href="{{ asset('/storage/images/removebg-logo.png') }}"
-        sizes="any"
-        type="image/svg+xml"
-        id="favicon"
-        rel="icon"
-    >
+    <meta name="robots" content="noindex"/>
+    <link href="{{ asset('/storage/images/removebg-logo.png') }}" sizes="any" type="image/svg+xml" id="favicon" rel="icon">
 
     <!-- For Safari on iOS -->
     <meta name="theme-color" content="#21252a">
 @endpush
 
-<div class="h2 fw-light d-flex align-items-center">
-    <img class="img-fluid" src="/storage/images/removebg-logo.png" alt="logo">
-
+<div class="h2 fw-light d-flex align-items-center justify-content-center">
+    @guest
+        <img class="img-fluid w-50" src="/storage/images/removebg-logo.png" alt="logo">
+    @else
+        <img class="img-fluid" src="/storage/images/removebg-logo.png" alt="logo">
+    @endguest
 </div>
