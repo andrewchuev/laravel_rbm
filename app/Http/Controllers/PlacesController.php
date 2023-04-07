fc75fe5cec12856a7821d3fc2cd1889b001da742
Andrey Chuiev <andrey.chuiev@dteam.dev>
2023-04-07 21:02:22 +0300

    Update visits query

--- app/Http/Controllers/PlacesController.php
+++ app/Http/Controllers/PlacesController.php
@@ -14,7 +14,7 @@ class PlacesController extends Controller
     }
 
     public function getVisits() {
-        return Visit::select(DB::raw("driver_id, any_value(place_id) as place_id, any_value(latitude) as latitude, any_value(longitude) as longitude, max(created_at) as last_date"))
+        return Visit::select(DB::raw("driver_id, max(place_id) as place_id, max(latitude) as latitude, max(longitude) as longitude, max(created_at) as last_date"))
             ->with(['driver', 'place'])
             ->groupBy('driver_id')->get();
     }
